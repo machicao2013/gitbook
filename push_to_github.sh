6aa1fd7742d35b7036a1ea29ba99d584afa8ecaf
maxingsong <maxingsong@xunlei.com>
2015-03-10 11:09:50 +0800

    commit leveldb and network

--- push_to_github.sh
+++ push_to_github.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
-test -d _book && rm -rf _book/*
-
 gitbook build .
 
 test -d book_end || mkdir book_end
 
 cp -r _book/* book_end/
 
-cd book_end
-
-git pull origin gh-pages
+git checkout gh-pages
 
 git add .
 
 git commit -a -m "publish"
-
-git push origin gh-pages
